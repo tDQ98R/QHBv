6464d4f946d93aada3407645973e622c693b7294
tDQ98R <dwpw78598@gmail.com>
2022-09-02 21:33:13 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts; 
     console.log("fetching nfts");
-    const api_key = 'pcfPgxmVcb68hs2DN-i-1i83BRKn3NCR'
+    const api_key = "Tpbt5xhjo1BxrMQUpakci_t9AXVNLFar"
 
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
@@ -41,7 +41,8 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = 'pcfPgxmVcb68hs2DN-i-1i83BRKn3NCR'
+      const api_key = "Tpbt5xhjo1BxrMQUpakci_t9AXVNLFar"
+
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
       const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
